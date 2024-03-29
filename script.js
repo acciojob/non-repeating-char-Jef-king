d7c171571052732e81b21f854c848aa7b1b3b5a0
Jef-king <147884623+Jef-king@users.noreply.github.com>
2024-03-30 00:49:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function firstNonRepeatedChar(str) {
- // Write your code here
+function firstNonRepeatedChar(x) {
+    const c = new Map();
+	
+    for (const char of x) {
+        c.set(char, (c.get(char) || 0) + 1);
+    }
+	
+    for (const char of x) {
+        if (c.get(char) === 1) {
+            return char;
+        }
+    }
+    return null;
 }
+
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
